098824a4d1cd4498ab49dc5cbfe8a6923c8381e8
JaeikBae <jaeik@node1.cluster.local>
2024-05-15 09:54:27 +0900

    add pwd

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 sudo docker build -t ollama .
 sudo docker run --rm -d \
-	-v ./data/root:/root \
-	-v ./data/db:/ws/open-webui/backend/data \
+	-v $(pwd)/data/root:/root \
+	-v $(pwd)/data/db:/ws/open-webui/backend/data \
 	--name ollama_serve \
 	--gpus=all \
 	--network host \
